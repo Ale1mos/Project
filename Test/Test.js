3d05053b41a7136ae8ad7f89239413dbd12a9fd2
Alejandra Mosqueira <alejandramosqueirazapata1@gmail.com>
2023-12-18 00:06:40 -0500

    DropDown

--- Test/Test.js
+++ Test/Test.js
@@ -1,11 +1,29 @@
 const {Builder, By, Key} = require('selenium-webdriver');
+// const { Select } = require('selenium-webdriver/common');
 
-let driver = new Builder().forBrowser('chrome').build();
 
-await driver.get('https://www.saucedemo.com/');
+(async function example(){
+    let driver = new Builder().forBrowser('chrome').build();
 
-// PRIMERA VISTA / Login
-await driver.findElement(By.id('user-name')).sendKeys('standard_user');
-await driver.findElement(By.css('#password')).sendKeys('secret_sauce');
-await driver.findElement(By.css('[data-test="login-button"]')).click();
-await driver.executeScript('console.log("1");');
+    await driver.get('https://www.saucedemo.com/');
+
+    // PRIMERA VISTA / Login
+    await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+    await driver.findElement(By.css('#password')).sendKeys('secret_sauce');
+    await driver.findElement(By.css('[data-test="login-button"]')).click();
+    await driver.executeScript('console.log("1");');
+
+    const dropdownElement = await driver.findElement(By.className('product_sort_container'));
+
+    // Hacer clic en la lista desplegable para abrirla
+    await dropdownElement.click();
+
+    // Simular teclas de flecha hacia abajo para navegar a la segunda opción
+    await dropdownElement.sendKeys(Key.ARROW_DOWN);
+
+    // Simular tecla Enter para seleccionar la opción
+    await dropdownElement.sendKeys(Key.ENTER);
+    await driver.sleep(8000)
+
+    await driver.executeScript('console.log("2");');
+})();
